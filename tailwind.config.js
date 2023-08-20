ac79ff788d02fd3a38efb61e5d81187663f4b388
Arshdeep singh <arsh199820@gmail.com>
2023-08-20 18:27:00 +0530

    refactor: typo

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
+    colors: {
+      primary: {
+        100: "#ebdcf8",
+        200: "#d8b8f1",
+        300: "#c495eb",
+        400: "#b171e4",
+        500: "#9d4edd",
+        600: "#7e3eb1",
+        700: "#5e2f85",
+        800: "#3f1f58",
+        900: "#1f102c",
+      },
+      Grape: "#6633CC",
+      Byzantine: "#BF2CBA",
+      PersianBlue: "#312CBF",
+    },
     extend: {},
   },
   plugins: [],
 };
-
