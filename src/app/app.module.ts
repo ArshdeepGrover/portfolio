bef825aa5106554f2505ed998dcbed574567bf56
Arshdeep singh <arsh199820@gmail.com>
2023-09-09 20:51:42 +0530

    feat: add required components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SkillsComponent } from './skills/skills.component';
+import { SocialMediaComponent } from './social-media/social-media.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HeaderComponent,
+    AboutMeComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    SocialMediaComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
