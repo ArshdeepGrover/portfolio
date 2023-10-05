d14102f5ba744dd326c97c2b503005a07fd04e2f
Arshdeep singh <arsh199820@gmail.com>
2023-10-05 11:29:03 +0530

    feat: add skills

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portfolio';
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private viewportScroller: ViewportScroller
+  ) {}
+
+  ngOnInit(): void {
+    this.checkFragment();
+  }
+
+  checkFragment() {
+    this.activatedRoute.fragment.subscribe((fragment) => {
+      if (fragment) {
+        this.scrollAuto(fragment);
+      }
+    });
+  }
+
+  scrollAuto(elementId: string): void {
+    this.viewportScroller.scrollToAnchor(elementId);
+  }
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  highlight!: any;
+  constructor(private activatedRoute: ActivatedRoute) {}
+  ngOnInit(): void {
+    this.checkFragment();
+  }
 
+  checkFragment() {
+    this.activatedRoute.fragment.subscribe((fragment) => {
+      this.highlight = fragment;
+    });
+  }
 }
