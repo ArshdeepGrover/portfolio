74dc6eb492fa51b834063e258098e3b69981df46
Arshdeep Singh <arsh199820@gmail.com>
2024-03-30 20:26:50 +0530

    refactor: remove works

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -19,10 +19,10 @@ export class NavbarComponent implements OnInit {
       name: 'Skills',
       fragment: 'skills',
     },
-    {
-      name: 'Certification',
-      fragment: 'certification',
-    },
+    // {
+    //   name: 'Works',
+    //   fragment: 'projects',
+    // },
     {
       name: 'Experience',
       fragment: 'experience',
