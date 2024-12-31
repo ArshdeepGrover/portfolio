32d7f30f1bf5bbc927656bbf10f9933abfdfab3f
Arshdeep Singh <arsh199820@gmail.com>
2024-12-31 21:12:52 +0530

    feat: integrate typed.js for dynamic text animation in header component

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, AfterViewInit {
+  @ViewChild('typedElement') typedElement!: ElementRef;
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    const options = {
+      strings: ['a Developer', 'a Engineer'],
+      typeSpeed: 80,
+      backSpeed: 25,
+      loop: true,
+      smartBackspace: true,
+      fadeOut: false,
+      fadeOutClass: 'typed-fade-out',
+      fadeOutDelay: 500,
+      showCursor: false,
+    };
+
+    new Typed(this.typedElement.nativeElement, options);
+  }
 }
